f5fd355c44b8831f0e888467baac50f3edacd431
Fang Jian <fangjianmail@126.com>
2016-11-04 10:08:48 +0800

    html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var webpack = require("webpack");
+var path=require("path");
 
 module.exports={
   entry:{
     index:'./index.js'
   },
   output:{
+    path: path.join(__dirname,"public"),
     filename:'[name].bundle.js'
   },
   module:{
